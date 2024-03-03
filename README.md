f2f9bd00b3e5610b9553330ddb2948edd4c591fd
John Long <john@128.io>
2024-03-03 16:47:43 -0600

    Showcase how memory can be tracked

---
+++ README.md
@@ -0,0 +1,6 @@
+# Exercise: Tracking browser memory usage during Cypress E2E tests
+
+This repository contains exercises and resources for tracking browser 
+memory usage during Cypress end-to-end (E2E) tests. This can be used
+to track memory leaks and other performance issues that happen during
+high memory pressure.
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,37 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+/**
+ * Using the `window:before:load` event, we can inject a script into the
+ * browser to track memory usage. This is useful for detecting memory
+ * leaks and other issues that can be caused by excessive memory usage.
+ * 
+ * The script will run every 250ms and track the maximum memory usage
+ * of the JavaScript heap. When the browser window is unloaded, the
+ * maximum memory usage is logged to the console along with the spec
+ * file that was running. This could be written to a file or sent to
+ * a server for further analysis.
+ */
+Cypress.on('window:before:load', (win) => {
+    const script = `
+      window.maxMemory = 0;
+  
+      const processMemoryUsage = () => {
+        window.maxMemory = Math.max(window.maxMemory, performance.memory.usedJSHeapSize);
+      }
+
+      setInterval(processMemoryUsage, 250);
+  
+      window.getMemoryUsage = () => {
+        processMemoryUsage();
+        return window.maxMemory;
+      }
+    `;
+    win.eval(script);
+    win.onunload = () => {
+      const memory = win.eval('window.getMemoryUsage()');
+      console.log("Memory Usage (MB):", Number.parseInt(memory, 10) / 1024 / 1024);
+      console.log(Cypress.spec.relativeToCommonRoot);
+    }
+});
